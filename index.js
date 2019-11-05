bd66143b3b8c4dd3313db377e4ea2c05a6af4775
grayluker05 <graysonluker@gmail.com>
2019-11-05 10:49:05 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let count = 0;
+
+const counter = document.getElementById('counter');
+
+const listItems = document.getElementsByTagName('li');
+
+const orderedListItem = document.querySelector('.list-item');
+
+const orderedList = document.querySelectorAll('.list-item');
+
+console.log(orderedListItem);
+console.log(orderedList);
